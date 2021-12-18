a370d48c09a82e2dbaf3199718f465f0ccf4c959
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-12-18 16:33:43 +0900

    prepare for 1.0.1

--- src/index.js
+++ src/index.js
@@ -42,3 +42,9 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+require('update-electron-app')({
+  repo: 'github-user/repo',
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+})
